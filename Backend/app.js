7352ae2c25f4a305a215833faad0bf18ac4c00a3
Rahul <rahulkava024@gmail.com>
2026-08-25 12:41:53 +0530

    Updated the Usercontroller and User model to handle user registration and login functionality. Added new routes for user-related operations in userRoutes.js. Updated app.js to include the new user routes.

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,13 @@ require("dotenv").config();
 
 const connectDB = require("./config/db");
 const classroomRoutes = require("./routes/classroomRoutes");
+const userRoutes = require("./routes/UserRoutes");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 app.use(express.json());
+app.use("/api/users", userRoutes);
 app.get("/api/test", (req, res) => {
     res.send("API is working!");
 });
